b238222d525666f692b8abb979672ef1e93b8f07
Eemil Hartikainen <hartikainen-96@hotmail.com>
2020-04-03 13:24:34 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,35 +6,35 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AppComponent } from './app.component';
+
+import { IonicStorageModule } from '@ionic/storage'; //  Import Ionic Storage
+
+import { AngularFireModule } from '@angular/fire'; // Import Firebase Libraries
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth'; //  Import  FirebaseAuthentication
+import { environment } from '../environments/environment'; // Import Environment Variables
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+            BrowserModule,
+            IonicModule.forRoot(),
+            AppRoutingModule,
+            IonicStorageModule.forRoot(), // Import Ionic Storage Module
+            AngularFireModule.initializeApp(environment.firebase), // Import Firebase Modules
+            AngularFirestoreModule, // Import Firebase Modules
+            AngularFireDatabaseModule, // Import Firebase Modules
+            AngularFireAuthModule // Import Firebase Modules
+          ],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
-})
-
-@NgModule({
-  declarations: [...],
-  imports: [
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule,
-    AngularFirestoreModule
-  ],
-  providers: [AngularFirestoreModule],
-  bootstrap: [...]
 })
 export class AppModule {}
