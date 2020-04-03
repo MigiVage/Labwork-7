7e08c1daec7194ede36826285155a1f446d9e490
Eemil Hartikainen <hartikainen-96@hotmail.com>
2020-04-03 13:08:59 +0200

    app.module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,5 +24,17 @@ import { AppRoutingModule } from './app-routing.module';
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
+})
+
+@NgModule({
+  declarations: [...],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
+  ],
+  providers: [AngularFirestoreModule],
+  bootstrap: [...]
 })
 export class AppModule {}
