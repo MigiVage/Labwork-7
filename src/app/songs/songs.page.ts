b92f534839dd9ef5b27b4f2eb906c92234ebf89f
unknown <matiaskohanevic@gmail.com>
2020-04-03 15:56:16 +0300

    delete shit

--- src/app/songs/songs.page.ts
+++ src/app/songs/songs.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SongService } from '../services/song.service'; // Import SongService
+// import { SongService } from '../services/song.service'; // Import SongService
 
 @Component({
   selector: 'app-songs',
@@ -7,17 +7,17 @@ import { SongService } from '../services/song.service'; // Import SongService
   styleUrls: ['./songs.page.scss'],
 })
 export class SongsPage implements OnInit {
-  songs: Song[] = null;
+  // songs: Song[] = null;
 
-  constructor(private songService: SongService) { }
+  // constructor(private songService: SongService) { }
 
   ngOnInit() {
     // subscribe to valueChanges methon in SongList in songService
-    this.songService.get_SongList().valueChanges().subscribe(data => {
-      this.songs = []; // Initialize empty array
-      data.forEach(item => { // Loop items in collection
-        this.songs.push(item as Song); // Insert to array as song
-      });
-    });
+    // this.songService.get_SongList().valueChanges().subscribe(data => {
+    //   this.songs = []; // Initialize empty array
+    //   data.forEach(item => { // Loop items in collection
+    //     this.songs.push(item as Song); // Insert to array as song
+    //   });
+    // });
   }
 }
