69a7ee7ead9fdcdaa84857ab4539b9ce336cf170
Eemil Hartikainen <hartikainen-96@hotmail.com>
2020-04-03 14:06:59 +0200

    update

--- src/app/verify-email/verify-email.page.ts
+++ src/app/verify-email/verify-email.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from "../shared/authentication-service";
 
 @Component({
   selector: 'app-verify-email',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyEmailPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthenticationService
+  ) { }
 
   ngOnInit() {
   }
