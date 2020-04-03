8c2d0fcf88b14abcf2812cc3cfc396075edecd4f
Eemil Hartikainen <hartikainen-96@hotmail.com>
2020-04-03 13:03:52 +0200

    Firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDqSkLCgVxuss7sGZyI0vtnu-JJYueWtqM",
+    authDomain: "teamproject-399eb.firebaseapp.com",
+    databaseURL: "https://teamproject-399eb.firebaseio.com",
+    projectId: "teamproject-399eb",
+    storageBucket: "teamproject-399eb.appspot.com",
+    messagingSenderId: "650285258533",
+    appId: "1:650285258533:web:9b45055e3d4c77b61f6ccc",
+    measurementId: "G-Z72XZJ5RJY"
+  }
+  };
 
 /*
  * For easier debugging in development mode, you can import the following file
