2ab6d37ae1b788ce940c0fb9c4de7d6fabd860e3
Eemil Hartikainen <hartikainen-96@hotmail.com>
2020-04-03 14:55:58 +0200

    edit

--- src/app/songs/songs.page.ts
+++ src/app/songs/songs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SongService } from '../services/song.service'; // Import SongService
 
 @Component({
   selector: 'app-songs',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./songs.page.scss'],
 })
 export class SongsPage implements OnInit {
+  songs: Song[] = null;
 
-  constructor() { }
+  constructor(private songService: SongService) { }
 
   ngOnInit() {
+    // subscribe to valueChanges methon in SongList in songService
+    this.songService.get_SongList().valueChanges().subscribe(data => {
+      this.songs = []; // Initialize empty array
+      data.forEach(item => { // Loop items in collection
+        this.songs.push(item as Song); // Insert to array as song
+      });
+    });
   }
-
 }
